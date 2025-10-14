4c6a3d19f1cdc3075631f764e98127b0bde5b0d9
fatsermint <kyosti.ahti@gmail.com>
2025-10-14 09:39:01 +0300

    first commit

---
+++ sub_viewport_container.gd
@@ -0,0 +1,10 @@
+extends SubViewportContainer
+@onready var player: CollisionShape3D = $"../CharacterBody3D/CollisionShape3D"
+@onready var camera_3d: Camera3D = $SubViewport/Camera3D
+@onready var playerious: CharacterBody3D = $"../CharacterBody3D"
+
+
+
+func _process(delta: float) -> void:
+	camera_3d.position.x = playerious.position.x
+	camera_3d.position.z = playerious.position.z
