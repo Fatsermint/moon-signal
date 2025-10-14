5d86016985f95715c9b3373604f34ed4a04a22e1
fatsermint <kyosti.ahti@gmail.com>
2025-10-14 10:46:22 +0300

    added truck

---
+++ car.gd
@@ -0,0 +1,9 @@
+extends VehicleBody3D
+
+
+@export var MAX_STEER =0.9
+@export var ENGINE_POWER = 200
+
+func  _physics_process(delta: float) -> void:
+	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 2)
+	engine_force = Input.get_axis("down", "up") * ENGINE_POWER
